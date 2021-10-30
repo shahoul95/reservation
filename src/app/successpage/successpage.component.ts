064fb069a375f4ef5946c2dcae8f118d9d6d76a9
shahoul95 <shahoul95@gmail.com>
2021-10-31 00:24:06 +0200

    delete console

--- src/app/successpage/successpage.component.ts
+++ src/app/successpage/successpage.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-successpage',
@@ -9,7 +9,7 @@ import { Router } from '@angular/router';
 export class SuccesspageComponent implements OnInit {
   storagereservation: any;
   parsestoragereservation: any;
-  constructor(private router: Router) {
+  constructor() {
 
   }
 
